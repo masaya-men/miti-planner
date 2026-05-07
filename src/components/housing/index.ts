ec52db263571172914d377c1e9a5248385c7696f
masaya-men <masaya-men@users.noreply.github.com>
2026-05-07 23:07:43 +0900

    chore(housing): components/housing バレルエクスポート

---
+++ src/components/housing/index.ts
@@ -0,0 +1,8 @@
+// src/components/housing/index.ts
+/**
+ * housing コンポーネントのバレルエクスポート
+ * 外部からは `import { HousingComingSoonPage } from '@/components/housing'` で使う想定
+ */
+export { HousingComingSoonPage } from './HousingComingSoonPage';
+export { HousingDetailPagePlaceholder } from './HousingDetailPagePlaceholder';
+export { HousingTourPagePlaceholder } from './HousingTourPagePlaceholder';
